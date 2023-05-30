ab7ea36d856874ddd958626f6a404d6ff51d4e68
João <joaolelles7@gmail.com>
2023-05-30 16:28:02 -0300

    Criando o Repositório

---
+++ Execícios-1.1.py
@@ -0,0 +1,48 @@
+# Operações básicas
+# Exercício 1
+a = 10
+b = 5
+
+print(a + b)
+# 15
+print(a - b)
+# 5
+print(a * b)
+# 50
+print(a / b)
+# 2.0
+print(a // b)
+# 2
+print(a**b)
+# 100000
+print(a % b)
+# 0
+
+# Exercício 2
+hours = 6
+minutes = hours * 60
+seconds = minutes * 60
+print(hours)
+# 6
+print(minutes)
+# 360
+print(seconds)
+# 21600
+
+# Exercício 3
+# Opcional:
+x = 5
+print(x)
+
+# Obrigatório:
+x = 5
+y = 10
+z = x + y
+print(z)
+
+# Exercício 4
+books = 60
+book_price = (1 - 0.4) * 24.20
+logistic = 3 + (books - 1) * 0.75
+cost = books * book_price + logistic
+print(cost)
---
+++ sample.py
@@ -0,0 +1,3 @@
+my_age = 18
+print(my_age)
+team_names = ["Eli", "João", "Ann"]

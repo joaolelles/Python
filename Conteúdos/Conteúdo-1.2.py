5be773f26773e468912f11008216b1f7ed823dfb
João <joaolelles7@gmail.com>
2023-06-09 07:22:11 -0300

    add: Desempacotamento de valores

--- Conteúdos/Conteúdo-1.2.py
+++ Conteúdos/Conteúdo-1.2.py
@@ -23,3 +23,16 @@ import sys
 
 err = "Arquivo não encontrado"
 print(f"Erro aconteceu: {err}", file=sys.stderr)
+
+# Desempacotamento de valores
+a, b = "cd"
+print(a)  # saída: c
+print(b)  # saída: d
+
+head, *tail = (
+    1,
+    2,
+    3,
+)  # Quando um * está presente no desempacotamento, os valores são desempacotados em formato de lista.
+print(head)  # saída: 1
+print(tail)  # saída: [2, 3]

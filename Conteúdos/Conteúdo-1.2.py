df117f36152d21efdd1146a509441a9b8d045b16
João <joaolelles7@gmail.com>
2023-06-12 00:33:28 -0300

    add: Testes automatizados

--- Conteúdos/Conteúdo-1.2.py
+++ Conteúdos/Conteúdo-1.2.py
@@ -245,3 +245,24 @@ with open("graduacao_unb.csv", encoding="utf-8") as file:
         if department not in group_by_department:
             group_by_department[department] = 0
         group_by_department[department] += 1
+
+
+# Testes automatizados
+# codigo.py
+def is_odd(number):
+    "Retorna True se um número é ímpar, senão False."
+    return number % 2 != 0
+
+
+# test_codigo.py
+from codigo import is_odd
+
+
+def test_is_odd_when_number_is_odd_returns_true():
+    "Para um número ímpar a função deve retornar o valor True"
+    assert is_odd(3) is True
+
+
+def test_is_odd_when_number_is_even_returns_false():
+    "Para um número par a função deve retornar o valor False"
+    assert is_odd(2) is False

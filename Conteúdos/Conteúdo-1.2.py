71bc65e9bcb2e0f3c8fa21a640636e33601168e1
João <joaolelles7@gmail.com>
2023-06-11 23:57:05 -0300

    Manipulando arquivos CSV

--- Conteúdos/Conteúdo-1.2.py
+++ Conteúdos/Conteúdo-1.2.py
@@ -178,3 +178,70 @@ grass_type_pokemons = [
 with open("grass_pokemons.json", "w") as file:
     # escreve no arquivo já transformando em formato json a estrutura
     json.dump(grass_type_pokemons, file)
+
+
+# Manipulando arquivos CSV
+import csv
+
+with open("graduacao_unb.csv", encoding="utf-8") as file:
+    # Os valores padrão de 'delimiter' e 'quotechar' são os mesmos utilizados
+    # abaixo. Você pode removê-los ou alterá-los conforme necessidade
+    graduacao_reader = csv.reader(file, delimiter=",", quotechar='"')
+
+    # Usando o conceito de desempacotamento
+    header, *data = graduacao_reader
+
+print(data)
+
+# Podemos fazer uma análise e verificar quantos cursos são ofertados por departamento. Em seguida salvamos o resultado também no formato .csv:
+import csv
+
+with open("graduacao_unb.csv", encoding="utf8") as file:
+    graduacao_reader = csv.reader(file, delimiter=",", quotechar='"')
+    # Usando o conceito de desempacotamento
+    header, *data = graduacao_reader
+
+group_by_department = {}
+for row in data:
+    department = row[15]
+    if department not in group_by_department:
+        group_by_department[department] = 0
+    group_by_department[department] += 1
+
+# Escreve o relatório em .csv
+# Abre um arquivo para escrita
+with open("department_report.csv", "w", encoding="utf-8") as file:
+    writer = csv.writer(file)
+
+    # Escreve o cabeçalho
+    headers = [
+        "Departamento",
+        "Total de Cursos",
+    ]
+    writer.writerow(headers)
+
+    # Escreve as linhas de dados
+    # Desempacotamento de valores
+    for department, grades in group_by_department.items():
+        # Agrupa o dado com o turno
+        row = [
+            department,
+            grades,
+        ]
+        writer.writerow(row)
+
+# Existem ainda o leitor e o escritor baseados em dicionários. A principal vantagem é que não precisamos manipular os índices para acessar os dados das colunas. A desvantagem é o espaço ocupado em memória (que é maior que o comum), devido à estrutura de dados utilizada.
+import csv
+
+# lê os dados
+with open("graduacao_unb.csv", encoding="utf-8") as file:
+    graduacao_reader = csv.DictReader(file, delimiter=",", quotechar='"')
+
+    # A linha de cabeçalhos é utilizada como chave do dicionário
+    # agrupa cursos por departamento
+    group_by_department = {}
+    for row in graduacao_reader:
+        department = row["unidade_responsavel"]
+        if department not in group_by_department:
+            group_by_department[department] = 0
+        group_by_department[department] += 1
